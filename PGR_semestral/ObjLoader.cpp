a409276968741b33398c1ef0744e9d702d6c58a1
Amirkhan <myrzaami@student.cvut.cz>
2026-03-24 12:33:51 +0100

    ObjLoader optimized

--- PGR_semestral/ObjLoader.cpp
+++ PGR_semestral/ObjLoader.cpp
@@ -1,7 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS    
 #include "ObjLoader.h"
-#include <fstream>
 #include <iostream>
-#include <sstream>
+#include <stdio.h>
+#include <string.h>
 
 bool ObjLoader::loadOBJ(
     const std::string& path,
@@ -9,49 +10,58 @@ bool ObjLoader::loadOBJ(
     std::vector<glm::vec2>& out_uvs,
     std::vector<glm::vec3>& out_normals) {
 
-    std::ifstream file(path);
-    if (!file.is_open()) {
+    // Using fopen_s as Visual Studio considers standard fopen unsafe
+    FILE* file;
+    if (fopen_s(&file, path.c_str(), "r") != 0 || file == nullptr) {
         std::cout << "Failed to open file: " << path << std::endl;
         return false;
     }
 
-    std::string line;
     std::vector<glm::vec3> temp_vertices;
     std::vector<glm::vec2> temp_uvs;
     std::vector<glm::vec3> temp_normals;
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        std::string type;
-        iss >> type;
-        if (type == "v") {
+
+    char lineHeader[128];
+    // Read the first word of the line until the End Of File
+    while (fscanf(file, "%127s", lineHeader) != EOF) {
+
+        if (strcmp(lineHeader, "v") == 0) {
             glm::vec3 vertex;
-            iss >> vertex.x >> vertex.y >> vertex.z;
+            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
             temp_vertices.push_back(vertex);
         }
-        else if (type == "vt") {
-            glm::vec2 uv_array;
-            iss >> uv_array.x >> uv_array.y;
-            temp_uvs.push_back(uv_array);
+        else if (strcmp(lineHeader, "vt") == 0) {
+            glm::vec2 uv;
+            fscanf(file, "%f %f\n", &uv.x, &uv.y);
+            temp_uvs.push_back(uv);
         }
-        else if (type == "vn") {
-            glm::vec3 normal_vertex;
-            iss >> normal_vertex.x >> normal_vertex.y >> normal_vertex.z;
-            temp_normals.push_back(normal_vertex);
+        else if (strcmp(lineHeader, "vn") == 0) {
+            glm::vec3 normal;
+            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            temp_normals.push_back(normal);
         }
-        else if (type == "f") {
+        else if (strcmp(lineHeader, "f") == 0) {
             unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            char slash;
             
+            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                &vertexIndex[0], &uvIndex[0], &normalIndex[0],
+                &vertexIndex[1], &uvIndex[1], &normalIndex[1],
+                &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+            
+            if (matches != 9) {
+                std::cout << "File cannot be read by parser! Bad format!\n";
+                fclose(file);
+                return false;
+            }
+
             for (int i = 0; i < 3; i++) {
-                iss >> vertexIndex[i] >> slash >> uvIndex[i] >> slash >> normalIndex[i];
-                vertexIndex[i]--;
-                uvIndex[i]--;
-                normalIndex[i]--;
-                out_vertices.push_back(temp_vertices[vertexIndex[i]]);
-                out_uvs.push_back(temp_uvs[uvIndex[i]]);
-                out_normals.push_back(temp_normals[normalIndex[i]]);
+                out_vertices.push_back(temp_vertices[vertexIndex[i] - 1]);
+                out_uvs.push_back(temp_uvs[uvIndex[i] - 1]);
+                out_normals.push_back(temp_normals[normalIndex[i] - 1]);
             }
         }
     }
+
+    fclose(file);
     return true;
 }
